61d5d50d7f9a15055c9873dea07660aab10786ab
ikoltcov <ikoltcov@yandex.ru>
2022-10-25 15:13:50 +0300

    finish with contest

--- yandex_contest/D/main.py
+++ yandex_contest/D/main.py
@@ -1,11 +1,16 @@
 import sys
 
+def generate(current: str, open: int, closed: int, n: int):
+    if len(current) == n*2:
+        sys.stdout.write(f'{current}\n')
+        return
+
+    if open < n:
+        generate(current + '(', open + 1, closed, n)
+
+    if closed < open:
+        generate(current + ')', open, closed + 1, n)
+
 n = int(sys.stdin.readline())
-arr = [n * 2]
 
-arr[0] = '(' * n + ')' * n
-arr[-1] = '()' * n
-
-for i in range(1, n):
-    
-    pass
+generate('', 0, 0, n)
--- yandex_contest/G/main.py
+++ yandex_contest/G/main.py
@@ -1,4 +1,3 @@
-from queue import Queue
 from typing import List
 
 def distance(x1: int, y1: int, x2: int, y2: int) -> float:
@@ -7,40 +6,45 @@ def distance(x1: int, y1: int, x2: int, y2: int) -> float:
 def bfs(start: int, end: int, graph: List[List[int]]):
     cost = [-1] * len(graph)
     cost[start] = 0
-    queue = Queue()
-    queue.put(start)
+    queue = [start]
 
-    while not queue.empty():
-        v = queue.get()
+    while queue:
+        v = queue.pop(0)
         for w in graph[v]:
+            if w == end:
+                return cost[v] + 1
+
             if cost[w] == -1:
-                queue.put(w)
+                queue.append(w)
                 cost[w] = cost[v] + 1
 
     return cost[end]
 
+result = -1
+
 with open('input.txt', 'r') as _input:
-    with open('output.txt', 'w+') as _output:
-        n = int(_input.readline())
-        cities = []
+    n = int(_input.readline())
 
-        for i in range(n):
-            data = _input.readline().split(' ')
-            cities.append([ int(data[0]), int(data[1]) ])
+    cities = []
+    for i in range(n):
+        data = _input.readline().split(' ')
+        cities.append([ int(data[0]), int(data[1]) ])
 
-        k = int(_input.readline())
-        start_end = _input.readline().split(' ')
+    k = int(_input.readline())
+    start_end = _input.readline().split(' ')
 
-        start = int(start_end[0]) - 1
-        end = int(start_end[1]) - 1
+start = int(start_end[0]) - 1
+end = int(start_end[1]) - 1
 
-        graph = []
-        for i in range(n):
-            paths = []
-            for j in range(n):
-                if distance(cities[i][0], cities[i][1], cities[j][0], cities[j][1]) <= k:
-                    paths.append(j)
-            graph.append(paths)
+graph = []
+for i in range(n):
+    paths = []
+    for j in range(n):
+        if distance(cities[i][0], cities[i][1], cities[j][0], cities[j][1]) <= k:
+            paths.append(j)
+    graph.append(paths)
 
-        result = bfs(start, end, graph)
-        _output.write(f'{result}')
+result = bfs(start, end, graph)
+
+with open('output.txt', 'w+') as _output:
+    _output.write(f'{result}')
