5b687e48418c4d67e8ba4af3314d9c636f8e8cf9
ikoltcov <ikoltcov@yandex.ru>
2022-11-01 01:49:15 +0300

    WIP

--- yandex_contest.backend/H/Program.cs
+++ yandex_contest.backend/H/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-// using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
